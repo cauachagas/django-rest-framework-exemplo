6a4ddf0b70370445ca7436269bc10c4af82b4c59
Cauã Chagas <caua.geof@gmail.com>
2021-03-18 00:07:48 -0300

    Incluindo imagem no Model

--- livros/models.py
+++ livros/models.py
@@ -4,6 +4,10 @@ from uuid import uuid4
 # Create your models here.
 
 
+def upload_imagem_livro():
+    return f"{instance.id}-{filename}"
+
+
 class Livros(models.Model):
     id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
     titulo = models.CharField(max_length=255)
@@ -13,3 +17,5 @@ class Livros(models.Model):
     paginas = models.IntegerField()
     editora = models.CharField(max_length=255)
     criado = models.DateField(auto_now_add=True)
+    imagem = models.ImageField(
+        upload_to=upload_imagem_livro, height_field=None, width_field=None, max_length=100, blank=True, null=True)
