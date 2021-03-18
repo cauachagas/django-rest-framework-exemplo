47b625537b2e91e201dca596bda1c177e01cd68f
Cauã Chagas <caua.geof@gmail.com>
2021-03-18 00:13:59 -0300

    Modificando urls para incluir a pasta de imagens

--- meu_django_rest_api/urls.py
+++ meu_django_rest_api/urls.py
@@ -17,11 +17,13 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from livros.api import viewsets
+from django.conf.urls.static import static
+from django.conf import settings
 
 rota = routers.DefaultRouter()
 rota.register(r"livros", viewsets.LivrosViewSet, basename="Livros")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include(rota.urls))
-]
+    path('', include(rota.urls))
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
