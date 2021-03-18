69bd151fd50f884a22cd453bf4fb9ab4e762765c
Cauã Chagas <caua.geof@gmail.com>
2021-03-17 23:51:55 -0300

    Criando url

--- meu_django_rest_api/urls.py
+++ meu_django_rest_api/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+from livros.api import viewsets
+
+rota = routers.DefaultRouter()
+rota.register(r"livros", viewsets.LivrosViewSet, basename="Livros")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", include(rota.urls))
 ]
