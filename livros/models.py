1523e652007c9ecb85cdb5b86914f42418782ee7
Cauã Chagas <caua.geof@gmail.com>
2021-03-17 23:48:09 -0300

    Criando models

--- livros/models.py
+++ livros/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from uuid import uuid4
 
 # Create your models here.
+
+
+class Livros(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid4, editable=False)
+    titulo = models.CharField(max_length=255)
+    autor = models.CharField(max_length=255)
+    ano = models.IntegerField()
+    estado = models.CharField(max_length=50)
+    paginas = models.IntegerField()
+    editora = models.CharField(max_length=255)
+    criado = models.DateField(auto_now_add=True)
