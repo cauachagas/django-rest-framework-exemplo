614b08a85fd68635c253f3914743262f49417e75
Cauã da Silva Chagas dos Santos <caua.geof@gmail.com>
2021-05-06 20:22:42 -0300

    Mudança do nome do projeto

--- core/urls.py
+++ core/urls.py
@@ -1,4 +1,4 @@
-"""meu_django_rest_api URL Configuration
+"""core URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.1/topics/http/urls/
